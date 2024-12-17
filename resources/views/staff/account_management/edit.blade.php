23516dd4f70c0bf82568628eba6a1ceda76fda06
gaajendren <gaajendren123@gmail.com>
2024-12-17 23:57:22 +0800

    account_bug

--- resources/views/staff/account_management/edit.blade.php
+++ resources/views/staff/account_management/edit.blade.php
@@ -1,5 +1,5 @@
 <div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 min-h-[100%] ">
-    <div class="absolute top-0 left-0 bg-gray-700 w-full min-h-[110%] h-full opacity-60" style="bottom: auto;"></div>
+    <div class="absolute top-0 left-0 bg-gray-700 w-full min-h-[120%] h-full opacity-60" style="bottom: auto;"></div>
     <div class="relative p-4 w-full sm:max-w-md md:max-w-lg  max-w-md  max-h-full my-10">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
